7be1927c2f2b91b7238d51d16313eec37ace0ab8
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-15 20:47:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
   name_hash.sort_by { |_key, value| }.to_h
 end
+binding.pry
